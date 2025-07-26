ecde459b8affd92c0c95bab933f40f373c91f756
MD MAHMUDUR RAHMAN <105537287+Abirrahman2@users.noreply.github.com>
2025-07-26 11:30:58 +0600

    achievements, projects and contact info section added

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const sectionId = this.getAttribute('href').substring(1);
+        document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
